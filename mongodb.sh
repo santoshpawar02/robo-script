289963a1d5378997731f073c969fcafa37e63b24
santoshpawar02 <santosh.pawar02@gmail.com>
2025-02-12 14:36:08 +0530

    scripts edited

---
+++ mongodb.sh
@@ -0,0 +1,8 @@
+dnf install mongodb-org -y 
+
+systemctl enable mongod 
+systemctl start mongod 
+
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf
+
+systemctl restart mongod
---
+++ shipping.sh
@@ -0,0 +1,28 @@
+dnf install maven -y
+useradd roboshop
+
+mkdir /app 
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip 
+cd /app 
+unzip /tmp/shipping.zip
+
+cd /app 
+mvn clean package 
+mv target/shipping-1.0.jar shipping.jar 
+
+cp -r shipping.service /etc/systemd/system/shipping.service
+systemctl daemon-reload
+
+systemctl enable shipping 
+systemctl start shipping
+
+dnf install mysql -y 
+
+mysql -h localhost -uroot -pRoboShop@1 < /app/db/schema.sql
+
+mysql -h localhost -uroot -pRoboShop@1 < /app/db/app-user.sql 
+
+mysql -h localhost -uroot -pRoboShop@1 < /app/db/master-data.sql
+
+systemctl restart shipping
